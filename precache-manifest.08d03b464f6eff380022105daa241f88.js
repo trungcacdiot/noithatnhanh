07b4f68ebb6e4438e68944866580b0f98617752f
trungcacdiot <68458523+trungcacdiot@users.noreply.github.com>
2020-09-23 16:32:25 +0700

    Updates

---
+++ precache-manifest.08d03b464f6eff380022105daa241f88.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18a6911503a2819f6949def9ba982d27",
+    "url": "/noithatnhanh/index.html"
+  },
+  {
+    "revision": "9edb0bfa62b5e54038b0",
+    "url": "/noithatnhanh/static/css/main.5afa4ae2.chunk.css"
+  },
+  {
+    "revision": "c948161a7c0d11769d4e",
+    "url": "/noithatnhanh/static/js/2.97acc8e8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/noithatnhanh/static/js/2.97acc8e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9edb0bfa62b5e54038b0",
+    "url": "/noithatnhanh/static/js/main.225a3ff0.chunk.js"
+  },
+  {
+    "revision": "ae00e91fab681b3f1bd3",
+    "url": "/noithatnhanh/static/js/runtime-main.d7cf1261.js"
+  },
+  {
+    "revision": "dc880f17456d5cad7f07d83554c22b9b",
+    "url": "/noithatnhanh/static/media/chevron-left-solid.dc880f17.svg"
+  },
+  {
+    "revision": "1fce70541cd225507e29c233e23f2cb2",
+    "url": "/noithatnhanh/static/media/chevron-right-solid.1fce7054.svg"
+  },
+  {
+    "revision": "62d8b6ef8e6c796a5ddad822dff96874",
+    "url": "/noithatnhanh/static/media/facebook-brands.62d8b6ef.svg"
+  },
+  {
+    "revision": "276f52a709627c3d36a273e940fc93b2",
+    "url": "/noithatnhanh/static/media/google-plus-g-brands.276f52a7.svg"
+  },
+  {
+    "revision": "311b7fc943f6469bbe77fe30e939cfc2",
+    "url": "/noithatnhanh/static/media/heart-regular.311b7fc9.svg"
+  },
+  {
+    "revision": "639cc557cb298870daf69fbeaaa6e017",
+    "url": "/noithatnhanh/static/media/picture-1.639cc557.jpg"
+  },
+  {
+    "revision": "b916684b11239c16911ebe0bd892a732",
+    "url": "/noithatnhanh/static/media/picture-10.b916684b.jpg"
+  },
+  {
+    "revision": "83e4d826c930ff3cedfbd4296b0f9701",
+    "url": "/noithatnhanh/static/media/picture-2.83e4d826.jpg"
+  },
+  {
+    "revision": "d5378bab6cda9a910bcf34ac178ef1f6",
+    "url": "/noithatnhanh/static/media/picture-3.d5378bab.jpg"
+  },
+  {
+    "revision": "58ab4d69b9f45c9917fbba8ee6f977b0",
+    "url": "/noithatnhanh/static/media/picture-3jpg.58ab4d69.jpg"
+  },
+  {
+    "revision": "58bb29dbbc3b99b95e0286021bf5e635",
+    "url": "/noithatnhanh/static/media/picture-4.58bb29db.jpg"
+  },
+  {
+    "revision": "f9f04f7d1bcf493158273da41b457682",
+    "url": "/noithatnhanh/static/media/picture-5.f9f04f7d.jpg"
+  },
+  {
+    "revision": "869ee25c5c38c7703662ce5fae69b672",
+    "url": "/noithatnhanh/static/media/picture-6.869ee25c.jpg"
+  },
+  {
+    "revision": "a5a2a129abff844323f49fba45b6b2f0",
+    "url": "/noithatnhanh/static/media/picture-7.a5a2a129.jpg"
+  },
+  {
+    "revision": "27e381ef762bab659436c319ee32f1c7",
+    "url": "/noithatnhanh/static/media/picture-8.27e381ef.jpg"
+  },
+  {
+    "revision": "5616369fd186082488263372efdcb3ea",
+    "url": "/noithatnhanh/static/media/picture-9.5616369f.jpg"
+  },
+  {
+    "revision": "29def3e5dd923346cc9efa53b9bc0b65",
+    "url": "/noithatnhanh/static/media/user-regular.29def3e5.svg"
+  }
+]);
