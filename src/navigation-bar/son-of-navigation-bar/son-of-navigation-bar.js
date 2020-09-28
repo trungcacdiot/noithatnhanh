8b431c1636abad799c7ee7073d1f405310ffa687
trungcacdiot <68458523+trungcacdiot@users.noreply.github.com>
2020-09-28 16:27:21 +0700

    add

--- src/navigation-bar/son-of-navigation-bar/son-of-navigation-bar.js
+++ src/navigation-bar/son-of-navigation-bar/son-of-navigation-bar.js
@@ -7,11 +7,20 @@ class SonNavigation extends Component {
   render() {
     return (
       <div className="son-of-navigation">
-        <img className="picture1" src={Picture1} usemap="#workmap" alt="Workplace"/>
+        <img
+          className="picture1"
+          src={Picture1}
+          usemap="#workmap"
+          alt="Workplace"
+        />
         <map name="workmap">
-          <area className="area" shape="rect" coords="34,44,270,350" alt="Computer" href={Picture1}></area>
-          <area className="area" shape="rect" coords="290,172,333,250" alt="Phone" href={Picture1}></area>
-           <area className="area" shape="circle" coords="337,300,44" alt="Cup of coffee" href={Picture1}></area>
+          <area
+            className="area"
+            shape="circle"
+            coords="337,300,100"
+            alt="Cup of coffee"
+            href={Picture1}
+          ></area>
         </map>
         <div className="button-picture">
           <div className="content-picture">
@@ -22,11 +31,8 @@ class SonNavigation extends Component {
             <img className="left-icon" src={Left} />
             <img className="right-icon" src={Right} />
           </div>
-        
         </div>
-        
       </div>
-      
     );
   }
 }
